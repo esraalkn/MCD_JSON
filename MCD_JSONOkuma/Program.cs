51905c55684039e95a39788fe3cbf56ab8c0fb3b
ECR_EXCALIBUR_G770 <ECR_EXCALIBUR_G770@DESKTOP-TUMHS1A>
2021-12-22 21:13:16 +0300

    JSONOkuma

---
+++ MCD_JSONOkuma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MCD_JSONOkuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string JsonOkunanData = File.ReadAllText("C:\\JsonIslemlerim\\Personellerim.json");
+            List<Personel> Data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Personel>>(JsonOkunanData);
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(Data[i].Isim);
+            }
+            Console.ReadLine();
+        }
+    }
+}
